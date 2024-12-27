0316cd0057a0c8f261e78ffe86d893cf8e4b7d20
PinguLinux <alefunkyextra@gmail.com>
2024-12-27 14:03:23 +0100

    Ceiling should be 4 blocks wide, not 3

--- levels/backontrack/l1.h
+++ levels/backontrack/l1.h
@@ -1,4 +1,4 @@
 // Level Data Header
 #define BACKONTRACK_l1_TOTAL_BLOCKS 23031
-#define BACKONTRACK_l1_RLE_DATA_SIZE 12308
+#define BACKONTRACK_l1_RLE_DATA_SIZE 6772
 extern const unsigned short backontrack_l1_level_data[BACKONTRACK_l1_RLE_DATA_SIZE];
