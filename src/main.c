e8c02cbd187a0c1728138d30e8ffd459da27d2dc
PinguLinux <alefunkyextra@gmail.com>
2024-12-28 10:58:12 +0100

    Start pause->menu transition on dimmed palette

--- src/main.c
+++ src/main.c
@@ -143,7 +143,8 @@ void game_loop() {
         // If pressed start, pause the game
         if (key_hit(KEY_START)) {
             if (paused_routines()) {
-                game_state = STATE_MENU;
+                game_state = STATE_MENU;      
+                memcpy32(palette_buffer, pal_bg_mem, 256);
                 fade_out();
                 return;
             }
