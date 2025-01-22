7684148fd0a1ecb3353d55318b55737e733048bf
PinguLinux <alefunkyextra@gmail.com>
2025-01-22 10:41:30 +0100

    Ball orb oomph tweaks

--- levels/test2/SP.s
+++ levels/test2/SP.s
@@ -11,4 +11,10 @@ test2_spr_data:
    .hword 0x1 @ type
    .hword 0x20 @ bg layer 4 non rotated   
    .hword 0 @ rotation
+@ Object 2
+   .word 0x2f0 @ x
+   .hword 0x190 @ y
+   .hword 0x1a @ type
+   .hword 0x20 @ bg layer 4 non rotated   
+   .hword 0 @ rotation
    .byte 0xff
--- levels/xstep/SP.h
+++ levels/xstep/SP.h
@@ -1,3 +1,3 @@
 // Sprite data Header
-#define XSTEP_TOTAL_SPR 1264
+#define XSTEP_TOTAL_SPR 1267
 extern const unsigned short xstep_spr_data[(XSTEP_TOTAL_SPR * 2) + 1];
