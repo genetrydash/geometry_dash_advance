42dba5c475de339441defdbcd8df9a25a45e8154
PinguLinux <alefunkyextra@gmail.com>
2025-01-24 12:32:12 +0100

    More mini ship oomph

--- levels/test2/SP.s
+++ levels/test2/SP.s
@@ -17,4 +17,34 @@ test2_spr_data:
    .hword 0x1a @ type
    .hword 0x20 @ bg layer 4 non rotated   
    .hword 0 @ rotation
+@ Object 3
+   .word 0x4e0 @ x
+   .hword 0x1a0 @ y
+   .hword 0x2 @ type
+   .hword 0x20 @ bg layer 4 non rotated   
+   .hword 0 @ rotation
+@ Object 4
+   .word 0x4e0 @ x
+   .hword 0x1a0 @ y
+   .hword 0x36 @ type
+   .hword 0x20 @ bg layer 4 non rotated   
+   .hword 0 @ rotation
+@ Object 5
+   .word 0x4e0 @ x
+   .hword 0x1a0 @ y
+   .hword 0x1c @ type
+   .hword 0x20 @ bg layer 4 non rotated   
+   .hword 0 @ rotation
+@ Object 6
+   .word 0x540 @ x
+   .hword 0x1a0 @ y
+   .hword 0x1 @ type
+   .hword 0x20 @ bg layer 4 non rotated   
+   .hword 0 @ rotation
+@ Object 7
+   .word 0x540 @ x
+   .hword 0x1b0 @ y
+   .hword 0xa @ type
+   .hword 0x20 @ bg layer 4 non rotated   
+   .hword 0 @ rotation
    .byte 0xff
