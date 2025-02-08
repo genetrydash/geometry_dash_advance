ab8d970aab58929cb8eea8313542468abfd4d619
PinguLinux <alefunkyextra@gmail.com>
2025-02-08 11:49:45 +0100

    Fix level complete palette

--- src/main.c
+++ src/main.c
@@ -288,7 +288,7 @@ void game_loop() {
 
     memcpy16(palette_buffer, blockPalette, sizeof(blockPalette) / sizeof(COLOR));
     memcpy16(&palette_buffer[240], &menu_palette[16], 16);
-
+    
     memcpy32(&tile_mem_obj[0][992], level_text_chr, sizeof(level_text_chr) / 4);
     memcpy16(&palette_buffer[256], spritePalette, sizeof(spritePalette) / sizeof(COLOR));
 
