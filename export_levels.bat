01817e5000ecf4e555aca9e019626510ee18c4e8
PinguLinux <alefunkyextra@gmail.com>
2025-02-15 10:09:39 +0100

    Better level guide by KirikkSiSq

---
+++ export_levels.bat
@@ -0,0 +1,4 @@
+:: To add a level, add the name of the .json file (without the extension) to the list of levels
+:: Enter full path to your directory here
+CD /D "YOUR_DIRECTORY_PATH_HERE"
+export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything test test2
