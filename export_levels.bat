86bdbbac8119ee36abe0a140484e6a0d7bbbfd3f
PinguLinux <alefunkyextra@gmail.com>
2025-02-21 10:19:39 +0100

    Fix bat file

--- export_levels.bat
+++ export_levels.bat
@@ -1,4 +1,4 @@
 :: To add a level, add the name of the .json file (without the extension) to the list of levels
-:: Enter full path to your directory here
-CD /D "YOUR_DIRECTORY_PATH_HERE"
-export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything test test2
+@echo off
+py export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything electromanadventures clubstep test test2
+pause
